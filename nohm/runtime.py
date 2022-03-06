03627e620306e50c6786a4ecc2b62684766dbbca
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2022-03-06 12:54:59 -0500

    Add remaining MUX11 rules

--- nohm/runtime.py
+++ nohm/runtime.py
@@ -227,9 +227,7 @@ def reduce(main: MAIN) -> None:
     #   APP(BOT,x) -> BOT
     #   LAM(x,TOP) -> TOP
     #   LAM(x,BOT) -> BOT
-    # We add other affine rules to aid simplification.
-    #   MUX11(MUX?) -> MUX?
-    #   MUX?(MUX11) -> MUX?
+    # We additionally implement rules merging MUX11 into any other mux.
     # Note that when porting this to C, the C code should recycle terms
     # to reduce malloc overhead. See collect() calls in this function.
     # TODO handle levels in rules.
@@ -252,8 +250,12 @@ def reduce(main: MAIN) -> None:
                 stack.append((False, "out", safe(term.out)[-1]))
             elif isinstance(term, APP):
                 stack.append((True, port, term))
-                lhs_port, lhs = safe(term.lhs)
-                stack.append((False, lhs_port, lhs))
+                stack.append((False,) + safe(term.lhs))
+            elif isinstance(term, JOIN):
+                stack.append((True, port, term))
+                # Could the C runtime fork here?
+                stack.append((False,) + safe(term.lhs))
+                stack.append((False,) + safe(term.rhs))
             elif isinstance(term, LAM):
                 pass
             continue
@@ -301,7 +303,7 @@ def reduce(main: MAIN) -> None:
 
             # term = APP lhs rhs
             # lhs = MUX21 lhs1 lhs2
-            # ---------------------- APP-MUX aka APP-PAR
+            # ---------------------- APP-MUX21 aka APP-PAR
             # term = MUX21 app1 app2
             # rhs1, rhs2 = MUX12 rhs
             # app1 = APP lhs1 rhs1
@@ -337,7 +339,7 @@ def reduce(main: MAIN) -> None:
 
             # term = JOIN lhs rhs
             # lhs = TOP
-            # ------------------ JOIN-LHS-TOP
+            # ------------------ JOIN-TOP-left
             # term = TOP
             if isinstance(lhs, TOP):
                 link(term.pop("out"), term.pop("lhs"))
@@ -346,7 +348,7 @@ def reduce(main: MAIN) -> None:
 
             # term = JOIN lhs rhs
             # rhs = TOP
-            # ------------------ JOIN-RHS-TOP
+            # ------------------ JOIN-TOP-right
             # term = TOP
             if isinstance(rhs, TOP):
                 link(term.pop("out"), term.pop("rhs"))
@@ -355,7 +357,7 @@ def reduce(main: MAIN) -> None:
 
             # term = JOIN lhs rhs
             # lhs = BOT
-            # ------------------ JOIN-LHS-BOT
+            # ------------------ JOIN-BOT-left
             # term = rhs
             if isinstance(lhs, BOT):
                 link(term.pop("out"), term.pop("rhs"))
@@ -364,76 +366,156 @@ def reduce(main: MAIN) -> None:
 
             # term = JOIN lhs rhs
             # rhs = BOT
-            # ------------------ JOIN-RHS-BOT
+            # ------------------ JOIN-BOT-right
             # term = lhs
             if isinstance(rhs, BOT):
                 link(term.pop("out"), term.pop("lhs"))
                 collect(term)
                 continue
 
+        if isinstance(term, MUX11):
+            assert port == "out"
+            x_port, x = safe(term.in1)
+
+            # out = MUX11 x
+            # x = MUX11 in1
+            # --------------- MUX11-MUX11
+            # out = MUX11 in1
+            if isinstance(x, MUX11):
+                link(x.pop("out"), x.pop("in1"))
+                collect(x)
+                stack.append((False, "out", term))
+                continue
+
+            # out = MUX11 x
+            # x = MUX21 in1, in2
+            # ------------------- MUX11-MUX21
+            # out = MUX21 in1 in2
+            if isinstance(x, MUX21):
+                link(term.pop("out"), term.pop("in1"))
+                collect(term)
+                stack.append((False, x_port, x))
+                continue
+
+            # out1 = MUX11 x
+            # out2, x = MUX12 in1
+            # ---------------------- MUX11-MUX12
+            # out2, out1 = MUX12 in1
+            if isinstance(x, MUX12):
+                link(term.pop("out"), term.pop("in1"))
+                collect(term)
+                stack.append((False, x_port, x))
+                continue
+
+            # out1 = MUX11 x
+            # x, out2 = MUX12 in1
+            # ---------------------- MUX11-MUX12
+            # out1, out2 = MUX12 in1
+            if isinstance(x, MUX12):
+                link(term.pop("out"), term.pop("in1"))
+                collect(term)
+                stack.append((False, x_port, x))
+                continue
+
         if isinstance(term, MUX12):
-            if port != "out":
-                x_port, x = safe(term.in1)
+            assert port in ("out1", "out2")
+            x_port, x = safe(term.in1)
 
-                if isinstance(x, MUX21):
-                    assert x_port == "out"
+            # out1, out2 = MUX12 x
+            # x = JOIN lhs, rhs
+            # ------------------------- MUX12-JOIN
+            # out1 = JOIN lhs1 rhs1
+            # out2 = JOIN lhs2 rhs2
+            # lhs1, lhs2 = MUX12 lhs
+            # rhs1, rhs2 = MUX12 rhs
+            if isinstance(x, LAM):
+                assert x_port == "out"
+                lhs1, lhs2 = MUX12()(x.pop("lhs"))
+                rhs1, rhs2 = MUX12()(x.pop("rhs"))
+                JOIN()(lhs1, rhs1, term.pop("out1"))
+                JOIN()(lhs2, rhs2, term.pop("out2"))
+                collect(term)
+                continue
 
-                    # out1, out2 = MUX12 x
-                    # x = MUX21 in1 in2
-                    # -------------------- MUX-MUX-elim aka DUP-PAR (equal)
-                    # out1 = in1
-                    # out2 = in2
-                    if term.level == x.level:
-                        link(x.pop("in1"), term.pop("out1"))
-                        link(x.pop("in2"), term.pop("out2"))
-                        collect(term)
-                        continue
+            # out1, out2 = MUX12 x
+            # x = LAM var body
+            # ------------------------- MUX-LAM aka DUP-LAM
+            # out1 = LAM var1 body1
+            # out2 = LAM var2 body2
+            # var = MUX21 var1 var2
+            # body1, body2 = MUX12 body
+            if isinstance(x, LAM):
+                assert x_port == "out"
+                body1, body2 = MUX12()(x.pop("body"))
+                var1, out1 = LAM()(None, body1, term.pop("out1"))
+                var2, out2 = LAM()(None, body2, term.pop("out2"))
+                MUX21()(var1, var2, x.pop("var"))
+                collect(term)
+                continue
 
-                    # out1, out2 = MUX12 x
-                    # x = MUX21 in1 in2
-                    # -------------------- MUX-MUX-intro aka DUP-PAR (different)
-                    # out1 = MUX21 x11 x12
-                    # out2 = MUX21 x21 x22
-                    # x11, x21 = MUX12 in1
-                    # x12, x22 = MUX12 in2
-                    x11, x21 = MUX12()(x.pop("in1"))
-                    x12, x22 = MUX12()(x.pop("in2"))
-                    MUX21()(x11, x12, term.pop("out1"))
-                    MUX21()(x21, x22, term.pop("out2"))
+            # out1, out2 = MUX12 x
+            # x = MUX11 in1
+            # ---------------------- MUX12-MUX11
+            # out1, out2 = MUX12 in1
+            if isinstance(x, MUX11):
+                assert x_port == "out"
+                link(x.pop("out"), x.pop("in1"))
+                collect(x)
+                stack.append((False, port, term))
+                continue
+
+            if isinstance(x, MUX21):
+                assert x_port == "out"
+
+                # out1, out2 = MUX12 x
+                # x = MUX21 in1 in2
+                # -------------------- MUX-MUX-elim aka DUP-PAR (equal)
+                # out1 = in1
+                # out2 = in2
+                if term.level == x.level:
+                    link(x.pop("in1"), term.pop("out1"))
+                    link(x.pop("in2"), term.pop("out2"))
                     collect(term)
                     continue
 
                 # out1, out2 = MUX12 x
-                # x = JOIN lhs, rhs
-                # ------------------------- MUX-JOIN
-                # out1 = JOIN lhs1 rhs1
-                # out2 = JOIN lhs2 rhs2
-                # lhs1, lhs2 = MUX12 lhs
-                # rhs1, rhs2 = MUX12 rhs
-                if isinstance(x, LAM):
-                    assert x_port == "out"
-                    lhs1, lhs2 = MUX12()(x.pop("lhs"))
-                    rhs1, rhs2 = MUX12()(x.pop("rhs"))
-                    JOIN()(lhs1, rhs1, term.pop("out1"))
-                    JOIN()(lhs2, rhs2, term.pop("out2"))
-                    collect(term)
-                    continue
+                # x = MUX21 in1 in2
+                # -------------------- MUX-MUX-intro aka DUP-PAR (different)
+                # out1 = MUX21 x11 x12
+                # out2 = MUX21 x21 x22
+                # x11, x21 = MUX12 in1
+                # x12, x22 = MUX12 in2
+                x11, x21 = MUX12()(x.pop("in1"))
+                x12, x22 = MUX12()(x.pop("in2"))
+                MUX21()(x11, x12, term.pop("out1"))
+                MUX21()(x21, x22, term.pop("out2"))
+                collect(term)
+                continue
 
-                # out1, out2 = MUX12 x
-                # x = LAM var body
-                # ------------------------- MUX-LAM aka DUP-LAM
-                # out1 = LAM var1 body1
-                # out2 = LAM var2 body2
-                # var = MUX21 var1 var2
-                # body1, body2 = MUX12 body
-                if isinstance(x, LAM):
-                    assert x_port == "out"
-                    body1, body2 = MUX12()(x.pop("body"))
-                    var1, out1 = LAM()(None, body1, term.pop("out1"))
-                    var2, out2 = LAM()(None, body2, term.pop("out2"))
-                    MUX21()(var1, var2, x.pop("var"))
-                    collect(term)
-                    continue
+        if isinstance(term, MUX21):
+            assert port == "out"
+
+            # out = MUX21 x in2
+            # x = MUX11 in1
+            # ------------------- MUX21-MUX11-left
+            # out = MUX12 in1 in2
+            in1_port, in1 = safe(term.in1)
+            if isinstance(in1, MUX11):
+                link(in1.pop("out"), in1.pop("in1"))
+                collect(in1)
+                stack.append((False, port, term))
+                continue
+
+            # out = MUX21 in1 x
+            # x = MUX11 in2
+            # ------------------- MUX21-MUX11-right
+            # out = MUX12 in1 in2
+            in2_port, in2 = safe(term.in2)
+            if isinstance(in2, MUX11):
+                link(in2.pop("out"), in2.pop("in1"))
+                collect(in2)
+                stack.append((False, port, term))
+                continue
 
 
 ################################################################################
